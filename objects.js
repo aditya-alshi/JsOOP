4ab7f5ec3b4922d0d0d33d907f281c973aecf82e
aditya-alshi <141261627+aditya-alshi@users.noreply.github.com>
2024-01-09 03:18:16 +0000

    method chaining example

--- objects.js
+++ objects.js
@@ -8,4 +8,26 @@ class Character{
 
     time = 7;
     paid = 20000;
+
+    action(){
+        console.log(this.dialog);
+        return this;
+    }
+
+    cut(){
+        console.log(`${this.name} you are a ${this.role}. Now please do it again properly.`);
+        return this;
+    }
 }
+
+const actor1 = new Character(
+    "Monica",
+    "CatWomen",
+    "Look, it's either pink bunny or no bunny at all."
+);
+
+const actor2 = new Character(
+    "Chandler",
+    "Pink Bunny",
+    "No bunny at all. Always no bunny at all."
+);
