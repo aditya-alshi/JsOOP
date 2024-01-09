c8b140f7c811c65a65e905ac723f3a41af86d18c
Aditya Alshi <alshiaditya55@gmail.com>
2024-01-09 07:27:07 +0530

    Jsoop initial commit

---
+++ objects.js
@@ -0,0 +1,11 @@
+class Character{
+    
+    constructor(name, role, dialog){
+        this.name = name;
+        this.role = role;
+        this.dialog = dialog;
+    }
+
+    time = 7;
+    paid = 20000;
+}
